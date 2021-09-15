3f40879e98c1d893e15a94efdd51b4f990c5ef1c
Plastron <gudvinqwert@bk.ru>
2021-09-15 20:10:29 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+#a simple calculator
+
+
+while True:
+    num1 = int(input("Enter first number: "))
+    operation = str(input("Enter operation: "))
+    num2 = int(input("Enter second number: "))
+
+    if operation == '+':
+        print(num1 + num2)
+
+    elif operation == '-':
+        print(num1 - num2)
+
+    elif operation == '*':
+        print(num1 * num2)
+
+    elif operation == '/':
+        print(num1 / num2)
+
+        # check if user wants another calculation
+        # break the while loop if answer is no
+    next_calculation = input("Let's do next calculation? (yes/no): ")
+    if next_calculation == "no":
+        break
