ef102996f306c61367c2cb2bd8896d8fc5ff80d5
dmitr <Plastron>
2021-09-28 21:36:25 +0300

    #for conflict different var name and some comments

--- units_converter.py
+++ units_converter.py
@@ -1,23 +1,27 @@
-#empty for now
 
+#some history
 print("Hello! This is a humorous unit converter that converts from the \"Potrzebie System of Weights and Measures\" \ndeveloped by 19-year-old Donald E. Knuth, later a famed computer scientist, \ninto the metric system units and vice versa. According to Knuth, \nthe basis of this new revolutionary system is the potrzebie, \nwhich equals the thickness of Mad issue 26, or 2.263348517438173216473 mm. \nYou can visit https://en.wikipedia.org/wiki/List_of_humorous_units_of_measurement#Potrzebie for more information")
 
+#the table from https://polaris93.livejournal.com/2046896.html
 table_request = input("Do you want to see the Potrzebie table of units? (yes/no): ")
 if table_request == "yes":
     print("\n1 potrzebie = 2.2633 mm \n1 Furshlugginer potrzebie = 2.2633 km \n1 cm = 4.4182 potrzebie \n1 km = 0.44182 Fur-potrzebie \n1 wood = 144 minutes \n1 kovac = 0.864 seconds \n1 year = 3.6524 Cowznofskis \n1 watt = 3.499651 Whatmeworries")
 
 units_check_list = ["year", "month", "day", "hour", "minute", "second", "km", "m", "cm", "mm", "W"]
-while True:
 
+
+while True:
     print("You can convert time (y, w, d, h, m, s), length (km, m, cm, mm), and power (W). \nPlease enter the decimal number you want to convert, then whitespace, and then unit how it was shown in the previous sentence.")
+    #check if there are two separate variables in the input
     try:
-        num, unit = input("number unit").split(' ')
+        value, unit = input("number unit").split(' ')
     except:
         print("looks like you forgot about the whitespace")
         continue
 
+    #check if the first value is a number
     try:
-        num = int(num)
+        value = int(value)
     except:
         print("looks like your number is not decimal")
         continue
@@ -25,28 +29,28 @@ while True:
     outer_unit = 0
     if unit in units_check_list:
         if unit == "y":
-            print("%s y = %d clarke (cl)" % (num, num))
+            print("%s y = %d clarke (cl)" % (value, value))
         elif unit == "w":
-            print("%s w = %d kovac (kv)" % (num, num * 1.917))
+            print("%s w = %d kovac (kv)" % (value, value * 1.917))
         elif unit == "d":
-            print("%s d = %d kovac (kv)" % (num, num * 0.273))
+            print("%s d = %d kovac (kv)" % (value, value * 0.273))
         elif unit == "h":
-            print("%s h = %d wolverton (wl)" % (num, num * 11.414))
+            print("%s h = %d wolverton (wl)" % (value, value * 11.414))
         elif unit == "m":
-            print("%s m = %d wolverton (wl)" % (num, num * 0.18))
+            print("%s m = %d wolverton (wl)" % (value, value * 0.18))
         elif unit == "conflict":
-            print("%s conflict = %d conflict (wl)" % (num, num * 0.0031))
+            print("%s conflict = %d conflict (wl)" % (value, value * 0.0031))
             print("conflict")
         elif unit == "km":
-            print("%s km = %d Fur-potrzebie" % (num, num * 0.44181))
+            print("%s km = %d Fur-potrzebie" % (value, value * 0.44181))
         elif unit == "m":
-            print("%s m = %d Fur-potrzebie" % (num, num * 441.81))
+            print("%s m = %d Fur-potrzebie" % (value, value * 441.81))
         elif unit == "cm":
-            print("%s cm = %d potrzebie" % (num, num * 4.4181))
+            print("%s cm = %d potrzebie" % (value, value * 4.4181))
         elif unit == "mm":
-            print("%s mm = %d centipotrzebie" % (num, num * 4.4181))
+            print("%s mm = %d centipotrzebie" % (value, value * 4.4181))
         elif unit == "W":
-            print("%s W = %d Whatmeworries" % (num, num * 3.499650))
+            print("%s W = %d Whatmeworries" % (value, value * 3.499650))
     else:
         print("looks like the unit is not the one i can convert from")
 
